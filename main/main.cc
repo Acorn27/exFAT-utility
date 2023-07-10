4e473900733d8b841e93828b5e41a3f407c8250d
uta1002092849 <mmt2849@mavs.uta.edu>
2023-07-10 12:49:38 -0500

    debug main

--- main/main.cc
+++ main/main.cc
@@ -4,9 +4,14 @@ Project: exFAT utility - modified version
 Modification: 07/10/2023
 */
 #include <iostream>
+#include "lib/parse-command.h"
 
 int main(int argc, char** argv)
 {
-    std::cout << "hello world" << std::endl;
+    command* command_ptr = parse(argc, argv);
+    std::cout << "input_file "<< command_ptr->infileName << std::endl;
+    std::cout << "output_file "<< command_ptr->outfileName << std::endl;
+
+    free(command_ptr);
     return 0;
 }
