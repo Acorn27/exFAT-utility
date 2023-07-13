c267ab7ab0bc45884b767a56e9f2493a9e49c4ca
Acorn27 <maitran.mt02@gmail.com>
2023-07-13 01:22:47 +0100

    fix memory leak

--- lib/parse-command.cc
+++ lib/parse-command.cc
@@ -1,4 +1,4 @@
-#include "lib/parse-command.h"
+#include "parse-command.h"
 #include <iostream>
 #include <getopt.h>
 #include <string.h>
@@ -6,7 +6,7 @@
 command *parse(int argc, char **argv)
 {
     // dynamically alocated command structure
-    command *command_ptr = new command;
+    command *command_ptr = new command {};
 
     // counter for getopt
     int c;
@@ -14,23 +14,23 @@ command *parse(int argc, char **argv)
 
     // <option>: => there is an string follow an options. EX (-i inputFile.txt)
     // <option>  => yes/no option like verify
-    while ((c = getopt(argc, argv, "i:o:d:hvepc")) != -1)
+    while ((c = getopt(argc, argv, "i:o:d:chvep")) != -1)
     {
         switch (c)
         {
         case 'i':
             command_ptr->infile = 1;
-            command_ptr->infileName = new char[sizeof(optarg)];
+            command_ptr->infileName = new char[strlen(optarg) + 1];
             strcpy(command_ptr->infileName, optarg);
             break;
         case 'o':
             command_ptr->outfile = 1;
-            command_ptr->outfileName = new char[sizeof(optarg)];
+            command_ptr->outfileName = new char[strlen(optarg) + 1];
             strcpy(command_ptr->outfileName, optarg);
             break;
         case 'd':
             command_ptr->del_file = 1;
-            command_ptr->deleteFile = new char[sizeof(optarg)];
+            command_ptr->deleteFile = new char[strlen(optarg) + 1];
             strcpy(command_ptr->deleteFile, optarg);
             break;
         case 'c':
@@ -55,10 +55,26 @@ command *parse(int argc, char **argv)
                 std::cout << "Unkown option " << char(optopt) << std::endl;
             return (NULL);
         default:
-            break;
             return (NULL);
         }
     }
 
     return command_ptr;
+}
+
+void clean_up(command* cmd)
+{
+    if (cmd == nullptr)
+        return;
+
+    if (cmd->infileName != nullptr)
+        delete[] cmd->infileName;
+
+    if (cmd->outfileName != nullptr)
+        delete[] cmd->outfileName;
+
+    if (cmd->deleteFile != nullptr)
+        delete[] cmd->deleteFile;
+
+    delete cmd;
 }
