8a034e3820f87c9ddae69119a73591d0cd699971
Acorn27 <maitran.mt02@gmail.com>
2023-07-13 22:35:43 +0100

    automated leak-check using valgrind

---
+++ example/valgrind_leak_check.bash
@@ -0,0 +1,15 @@
+# create mock exfat image
+bash create_image.bash
+
+# move to paren directory
+cd ..
+
+# build exfat
+bazel build main:exfat
+
+# calling valgrind for leak check
+valgrind --leak-check=full bazel-bin/main/exfat -i images/exfat.img -o doncare.txt -c
+
+#clean up
+cd example
+bash cleanup_image.bash
