dd2c014498ca06b8cb6dd5118e9b95fc6999e589
Huy <93058128+Acorn27@users.noreply.github.com>
2023-07-29 19:39:28 +0000

    basic unittest for verify function

--- test/verify-unittest.cc
+++ test/verify-unittest.cc
@@ -2,8 +2,27 @@
 #include "verify-image.h"
 #include <iostream>
 
-TEST (copyImageTest, invalidArguments)
+char imageName[] = "images/exfat.img";
+
+// test for null argument
+TEST (verify_image, invalidArguments)
 {
-    int result = copy(NULL, NULL);
+    int result = verify_image(NULL);
     EXPECT_EQ(1, result);
 }
+
+// expected pass by using unmodified/new generated image
+// TEST(verify_image, expectedPassed)
+// {
+//     std::cout << "Create a fresh image first before run this test" << std::endl;
+//     int result = verify_image(imageName);
+//     EXPECT_EQ(1, result);
+// }
+
+// TEST(verify_image, expectedFailed)
+// {   
+//     // expected failed by modify backup region of image
+//     modified_image(imageName);
+//     int result = verify_image(imageName);
+//     EXPECT_EQ(0, result);
+// }
