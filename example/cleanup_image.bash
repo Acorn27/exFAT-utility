00c6c9d99e98e2d4383d1ef5747cf52f0b6c601e
uta1002092849 <mmt2849@mavs.uta.edu>
2023-07-28 22:43:56 -0500

    script now can run from project folder

--- example/cleanup_image.bash
+++ example/cleanup_image.bash
@@ -4,12 +4,12 @@ image_folder="images"
 mount_point="image_directory"
 
 # un mount image
-sudo umount ../"$image_folder"/"$mount_point"
+sudo umount "$image_folder"/"$mount_point"
 
 # clean up image and mounted folder
-rm -rf ../"$image_folder"/"$mount_point"
-rm ../"$image_folder"/*.img -f
-rmdir ../"$image_folder"
+rm -rf "$image_folder"/"$mount_point"
+rm "$image_folder"/*.img -f
+rmdir "$image_folder"
 
 # Additional information
 echo "exFAT image unmounted and removed successfully."
